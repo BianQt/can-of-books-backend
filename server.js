be932395d9de85b6ca73cb0565573db916837d4b
BianQt <bayanqutshan@gmail.com>
2021-09-14 03:10:22 +0300

    lab12 updates - Sam

--- server.js
+++ server.js
@@ -8,19 +8,23 @@ const mongoose = require("mongoose");
 
 require("dotenv").config({ path: __dirname + "/.env" });
 app.use(cors());
+app.use(express.json()); 
+
 const PORT = process.env.PORT;
 const mongoUrl = process.env.MONGO_URL;
 const dbName = process.env.DB_NAME;
 
 mongoose.connect(`${mongoUrl}${dbName}`);
 const bookShelf = require("./helpers/books.shelf");
-const getBooks = require("./controllers/books.controller");
+const {getBooks, createBook, deleteBook} = require("./controllers/books.controller");
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
 app.get("/books", getBooks);
+app.post('/books', createBook);
+app.delete('/books/:book_id', deleteBook); 
 
 //To save the books just once.
 app.get("/shelf", (req, res) => {
@@ -28,5 +32,5 @@ app.get("/shelf", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log("Server running on port 3001.");
+  console.log("Server running on port 8080.");
 });
